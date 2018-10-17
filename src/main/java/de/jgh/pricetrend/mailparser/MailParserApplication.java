47c4883d6dfac1a6438264f1cdc45df3c3774530
jhi <johannes.hinkov@gmail.com>
2018-10-17 23:09:30 +0200

    accidently removed..

--- src/main/java/de/jgh/pricetrend/mailparser/MailParserApplication.java
+++ src/main/java/de/jgh/pricetrend/mailparser/MailParserApplication.java
@@ -3,10 +3,11 @@ package de.jgh.pricetrend.mailparser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-//@EnableScheduling
+@EnableScheduling
 public class MailParserApplication {
 
 	public static void main(String[] args) {
