96cbfb52f843b5cc6805bed5589397ee6199b508
jhi <johannes.hinkov@gmail.com>
2018-11-01 18:26:47 +0100

    added some data to report

--- src/main/java/de/jgh/pricetrend/mailparser/ReportController.java
+++ src/main/java/de/jgh/pricetrend/mailparser/ReportController.java
@@ -1,5 +1,6 @@
 package de.jgh.pricetrend.mailparser;
 
+import de.jgh.pricetrend.mailparser.repo.DetailEntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,8 +14,16 @@ public class ReportController {
     @Autowired
     private PercentileCalcService percentileCalcService;
 
-    @GetMapping("/report/{model}")
-    public Object getReportForModelAndValues(@PathVariable(name = "model") String model, String mileage, String price, String registration) {
+    @Autowired
+    private DetailEntryRepository detailEntryRepository;
+
+    @GetMapping("/report/{model}/{mileage}/{registration}/{price}")
+    public Object getReportForModelAndValues(
+            @PathVariable(name = "model") String model,
+            @PathVariable(name = "mileage") String mileage,
+            @PathVariable(name = "registration") String registration,
+            @PathVariable(name = "price") String price
+    ) {
         HashMap<String, String> report = new HashMap<>();
         CalculatedPercentile mileagePercentile = percentileCalcService.mileagePercentile(model, mileage);
         CalculatedPercentile pricePercentile = percentileCalcService.pricePercentile(model, price);
@@ -24,6 +33,10 @@ public class ReportController {
         report.put("mileagePercentile", mileagePercentile.getLabel().name());
         report.put("pricePercentile", pricePercentile.getLabel().name());
         report.put("registrationPercentile", registrationPercentile.getLabel().name());
+
+        report.put("minPrice", detailEntryRepository.findTopByModelOrderByPriceAsc(model).getPrice().toString());
+        report.put("maxPrice", detailEntryRepository.findTopByModelOrderByPriceDesc(model).getPrice().toString());
+        report.put("avgPrice", detailEntryRepository.getAveragePrice(model).toString());
         return report;
     }
 }
--- src/main/java/de/jgh/pricetrend/mailparser/repo/DetailEntryRepository.java
+++ src/main/java/de/jgh/pricetrend/mailparser/repo/DetailEntryRepository.java
@@ -3,6 +3,8 @@ package de.jgh.pricetrend.mailparser.repo;
 import de.jgh.pricetrend.mailparser.model.DetailEntry;
 import de.jgh.pricetrend.mailparser.model.DetailEntryId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +12,8 @@ public interface DetailEntryRepository extends JpaRepository<DetailEntry, Detail
     List<DetailEntry> findByProcessed(boolean processed);
     DetailEntry findTopByProcessed(boolean processed);
     List<DetailEntry> findByIdInseratId(String inseratId);
+    DetailEntry findTopByModelOrderByPriceDesc(String model);
+    DetailEntry findTopByModelOrderByPriceAsc(String model);
+    @Query(value = "select avg(price) from detail_entry where model=:model", nativeQuery = true)
+    Double getAveragePrice(@Param("model") String model);
 }
